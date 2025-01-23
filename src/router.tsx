9b0eb13628df6f4a7997ae3ab7f791d94f310ac7
Leo en laptop <leonar2zb@gmail.com>
2025-01-23 17:12:42 -0300

    + Routing entre páginas

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './layouts/Layout'
-import Products from './views/Product'
+import Products from './views/Products'
 import NewProduct from './views/NewProduct'
 
 export const router = createBrowserRouter([
--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom"
 export default function NewProduct() {
-    return <div>
-        NewProduct
-    </div>
+    return (<>
+        <div className="flex justify-between">
+            <h2 className="text-4xl font-black text-slate-500">Registrar producto</h2>
+            <Link to='/'
+                className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500">
+                Regresar a productos
+            </Link>
+        </div>
+    </>)
 }
