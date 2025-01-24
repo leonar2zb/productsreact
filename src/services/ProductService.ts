f323398add6e4a092cafc1935c3035f88962b044
Leo en laptop <leonar2zb@gmail.com>
2025-01-24 16:12:20 -0300

    * Fix reinstalado react-router-dom + Instalado validbot para realizar validaciones + Validada la entrada de datos(name y price) antes hacer petición al API

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,22 @@
+import { DraftProductSchema } from "../types"
+import { safeParse } from "valibot"
+
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data: ProductData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price //convertir de cadena a número
+        })
+        if (result.success) {
+            console.log(result.output) // mostrar los datos validados
+        }
+        else
+            throw new Error('Datos no válidos')
+    } catch (error) {
+
+    }
 }
