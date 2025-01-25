cb6a70d0d3290e60981406a3f37c8a24018df8e8
Leo en laptop <leonar2zb@gmail.com>
2025-01-25 14:20:14 -0300

    + Creado componente para Editar a partir de Crear + Creada ruta para editar + Navegación a editar con Link

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -19,7 +20,12 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
                 {available}
             </td>
             <td className="p-3 text-lg text-gray-800 ">
-
+                <div className="flex gap-2 items-center">
+                    <Link to={`productos/${product.id}/edit`}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-sm text-center"
+                    >
+                        Editar</Link>
+                </div>
             </td>
         </tr>
     )
